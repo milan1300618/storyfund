ca80d64b805c48e04a70ba6cd2dbf12059859c10
milan1300618 <milan1300618@gmail.com>
2026-08-24 12:05:05 +0200

    Implement exception handling and logging
    Add error handling to log exceptions to a file.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from app import StoryFundApp
+import traceback
 
-if __name__ == "__main__":
-    StoryFundApp().run()
+try:
+    from app import StoryFundApp
+
+    if __name__ == "__main__":
+        StoryFundApp().run()
+
+except Exception:
+    try:
+        with open("/sdcard/StoryFund_crash.txt", "w", encoding="utf-8") as f:
+            traceback.print_exc(file=f)
+    except Exception:
+        pass
+    raise
